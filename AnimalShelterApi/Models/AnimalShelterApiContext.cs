06759ed54d94595aeca2d2255efece5226787d21
Kymani Stephens <kymanist00@gmail.com>
2023-06-09 16:21:02 -0700

    update db context file

---
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelterApi.Models;
+public class AnimalShelterApiContext : DbContext
+{
+  public DbSet<Animal> Animals { get; set; }
+
+  public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
+  {
+  }
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.Entity<Animal>()
+    .HasData(
+
+        new Animal { AnimalId = 1, Name = "Max", Species = "Cat", Age = 1, Breed = "Ragdoll", Status = "Available" }
+
+    );
+
+  }
+}
