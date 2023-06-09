089f641c3c0a3513646a0296d086065e5c6356f6
Kymani Stephens <kymanist00@gmail.com>
2023-06-09 16:20:10 -0700

    add class Animal

---
+++ AnimalShelterApi/Models/Animal.cs
@@ -0,0 +1,14 @@
+namespace AnimalShelterApi.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+    public string Breed { get; set; }
+    public string Status { get; set; }
+
+
+  }
+}
