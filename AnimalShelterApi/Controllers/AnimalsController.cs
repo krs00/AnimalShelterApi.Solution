c6e883e875d551eea6a3e99e840a37db8d334c3b
Kymani Stephens <kymanist00@gmail.com>
2023-06-11 05:42:40 -0700

    add DELETE endpoint for deleting animal info

--- AnimalShelterApi/Controllers/AnimalsController.cs
+++ AnimalShelterApi/Controllers/AnimalsController.cs
@@ -79,6 +79,21 @@ public class AnimalsController : ControllerBase
     return _db.Animals.Any(e => e.AnimalId == id);
   }
 
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeleteAnimal(int id)
+  {
+    Animal animal = await _db.Animals.FindAsync(id);
+    if (animal == null)
+    {
+      return NotFound();
+    }
+
+    _db.Animals.Remove(animal);
+    await _db.SaveChangesAsync();
+
+    return NoContent();
+  }
+
 
 
 
