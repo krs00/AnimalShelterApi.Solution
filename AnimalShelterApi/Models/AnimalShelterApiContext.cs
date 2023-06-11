0c69e02ea3bfe7d4ef32ed2128a011ff43d5d3b8
Kymani Stephens <kymanist00@gmail.com>
2023-06-10 18:01:23 -0700

    add seed data

--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -14,7 +14,14 @@ public class AnimalShelterApiContext : DbContext
     builder.Entity<Animal>()
     .HasData(
 
-        new Animal { AnimalId = 1, Name = "Max", Species = "Cat", Age = 1, Breed = "Ragdoll", Status = "Available" }
+        new Animal { AnimalId = 1, Name = "Max", Species = "Cat", Age = 1, Breed = "Ragdoll", Sex = "Male", Status = "Available" },
+				new Animal { AnimalId = 2, Name = "Luna", Species = "Cat", Age = 2, Breed = "Persian", Sex = "Female", Status = "Available" },
+				new Animal { AnimalId = 3, Name = "Lily", Species = "Cat", Age = 2, Breed = "Siamese", Sex = "Female", Status = "Available" },
+				
+
+				new Animal { AnimalId = 4, Name = "Bear", Species = "Dog", Age = 3, Breed = "Bulldog", Sex = "Male", Status = "Available" },
+				new Animal { AnimalId = 5, Name = "Duke", Species = "Dog", Age = 5, Breed = "Border Collie", Sex = "Male", Status = "Available" },
+				new Animal { AnimalId = 6, Name = "Milo", Species = "Dog", Age = 2, Breed = "Akita", Sex = "Male", Status = "Available" }
 
     );
 
