3bff4b9020c779a5ad171a4f28b86af396743438
Kymani Stephens <kymanist00@gmail.com>
2023-06-11 06:43:44 -0700

    add model validation for animal info

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AnimalShelterApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Species { get; set; }
     public int Age { get; set; }
     public string Breed { get; set; }
+    [Required]
     public string Sex { get; set; }
+    [Required]
     public string Status { get; set; }
 
 
