598daa1e7f77a04c269b94d2de9a30f44a6f8c2d
Kymani Stephens <kymanist00@gmail.com>
2023-06-10 21:06:53 -0700

    add GET endpoints for getting animal(s)

--- AnimalShelterApi/Controllers/AnimalsController.cs
+++ AnimalShelterApi/Controllers/AnimalsController.cs
@@ -4,16 +4,38 @@ using AnimalShelterApi.Models;
 
 namespace AnimalShelterApi.Controllers;
 
-  [Route("api/[controller]")]
-  [ApiController]
-  public class AnimalsController : ControllerBase
-  {
-    private readonly AnimalShelterApiContext _db;
+[Route("api/[controller]")]
+[ApiController]
+public class AnimalsController : ControllerBase
+{
+  private readonly AnimalShelterApiContext _db;
 
-    public AnimalsController(AnimalShelterApiContext db)
+  public AnimalsController(AnimalShelterApiContext db)
+  {
+    _db = db;
+  }
+
+
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<Animal>>> GetAsync()
+  {
+    return await _db.Animals.ToListAsync();
+  }
+
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Animal>> GetAnimalAsync(int id)
+  {
+    Animal animal = await _db.Animals.FindAsync(id);
+
+    // ERROR HANDLING
+    if (animal == null)
     {
-      _db = db;
+      return NotFound();
     }
 
-
+    return animal;
   }
+
+
+}
