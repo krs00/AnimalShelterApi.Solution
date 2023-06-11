761d553a41e944f47abb2fe7925ad51247c15424
Kymani Stephens <kymanist00@gmail.com>
2023-06-11 15:48:23 -0700

    config JWTBearerDefaults

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,36 +1,34 @@
 using AnimalShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authenticaion.JwtBearer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add Services to the container
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticaionScheme)
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
   .AddJwtBearer(options =>
-                  {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                      ValidateIssuer = true,
-                      ValidateAudience = true,
-                      ValidateLiftime = true,
-                      ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                      ValidAudience = builder.Configuration["Jwt.Audience"],
-                      IssuerSigningKey = new SymmetricSecutriyKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                    };
-                  });
+  {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateLifetime = true,
+      ValidIssuer = builder.Configuration["Jwt:Issuer"],
+      ValidAudience = builder.Configuration["Jwt:Audience"],
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+  });
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AnimalShelterApiContext>(
-                  dbContextOptions => dbContextOptions
-                    .UseMySql(
-                      builder.Configuration["ConnectionStrings:DefaultConnection"],
-                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                    )
-                  )
-                );
+  dbContextOptions => dbContextOptions.UseMySql(
+    builder.Configuration["ConnectionStrings:DefaultConnection"],
+    ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+  )
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
