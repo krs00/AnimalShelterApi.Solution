9d87d01dd6abacebf1191e0d86c9216abc579d67
Kymani Stephens <kymanist00@gmail.com>
2023-06-17 20:14:37 -0700

    rework program cs

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,34 +1,18 @@
 using AnimalShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add Services to the container
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-  .AddJwtBearer(options =>
-  {
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-      ValidateIssuer = true,
-      ValidateAudience = true,
-      ValidateLifetime = true,
-      ValidIssuer = builder.Configuration["Jwt:Issuer"],
-      ValidAudience = builder.Configuration["Jwt:Audience"],
-      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-  });
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AnimalShelterApiContext>(
-  dbContextOptions => dbContextOptions.UseMySql(
-    builder.Configuration["ConnectionStrings:DefaultConnection"],
-    ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
-  )
-);
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"],
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -44,8 +28,6 @@ else
 {
   app.UseHttpsRedirection();
 }
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 
